98b8ab0ffde28e98343d1380a4689e61935de717
aagust11 <132393883+aagust11@users.noreply.github.com>
2025-10-07 07:56:25 +0200

    Revert "Add evaluation settings tab in configuration"

--- main.js
+++ main.js
@@ -113,9 +113,7 @@ function handleAction(action, element, event) {
         'close-learning-activity-criteria', 'go-to-competency-settings',
         'open-learning-activity-rubric', 'close-learning-activity-rubric', 'set-learning-activity-rubric-tab',
         'add-rubric-item', 'remove-rubric-item', 'move-rubric-item', 'set-rubric-score',
-        'filter-learning-activity-rubric-students', 'set-evaluation-tab', 'select-evaluation-class',
-        'update-evaluation-type', 'update-competencial-level-value', 'update-competencial-minimum',
-        'update-evaluation-max-not-achieved', 'update-term-evaluation-method'
+        'filter-learning-activity-rubric-students', 'set-evaluation-tab', 'select-evaluation-class'
     ];
     const forceRenderActions = ['toggle-rubric-not-presented', 'toggle-rubric-delivered-late'];
     const shouldForceRender = forceRenderActions.includes(action);
